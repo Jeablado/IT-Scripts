7baf1dfaf8031210243d9acdf7b313171661f8d0
Jeremie LOBSIGER 151 <jeremie.lobsiger@caf15.caf.fr>
2025-10-21 14:50:22 +0200

    Ajout du dossier identifiants (sans identifiants) et du script permettant de généré le fichier crypté .key contenant le mdp

---
+++ Identifiants/MDPkey.ps1
@@ -0,0 +1,11 @@
+﻿
+####################################  SCRIPT PERMETTANT DE CRYPTER LE MOT DE PASSE ####################################################
+#
+#                            LE MOT DE PASSE EST SAUVEGARDE SUR UN FICHIER KEY et CRYPTE
+#
+#              CELA PERMET UNE AUTO-AUTHENTIFICATION A EXHANGE ONLINE ET QUE LE SCRIPT SOIT AUTOMNOME 
+#  
+#                     IL EST POSSIBLE DE FAIRE UNE AUTHENTIFICATION EN DONNANT SON MOT DE PASSE
+Read-Host -Prompt “Entrer votre mot de passe Office365” -AsSecureString | ConvertFrom-SecureString | Out-File .\TENANTNAME.key
+#
+#######################################################################################################################################
